7ca8098b57d2847c01950936e5b711e110c1b510
Dan Buckwell <42476059+danbuckwell@users.noreply.github.com>
2019-12-20 13:40:04 +0000

    Update Example.py

--- Example.py
+++ Example.py
@@ -6,7 +6,7 @@ log_file = 'log.log'
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)s %(levelname)s: %(message)s',
+    format='%(asctime)s - %(module)s,%(lineno)d - %(levelname)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     handlers=[
         logging.StreamHandler(),
